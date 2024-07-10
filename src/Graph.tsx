5e923e4096b3dd997b27882586c26f6aa9f10e5f
jassijasit <jasveersingh182002@gmail.com>
2024-07-10 16:11:05 +0530

    Fix type error in Graph component and update DataManipulator

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -11,6 +11,17 @@ interface IProps {
 interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
+
+interface Row {
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | null,
+}
+
 class Graph extends Component<IProps, {}> {
   table: Table | undefined;
 
@@ -23,10 +34,13 @@ class Graph extends Component<IProps, {}> {
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
       timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -36,24 +50,36 @@ class Graph extends Component<IProps, {}> {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
       }));
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
-      );
+      const newData = DataManipulator.generateRow(this.props.data);
+      const transformedData = Graph.transformData(newData);
+      this.table.update(transformedData);
     }
+  }
+
+  private static transformData(data: Row[]): Record<string, (string | number | boolean | Date)[]> {
+    return data.reduce((acc, row) => {
+      Object.entries(row).forEach(([key, value]) => {
+        if (!acc[key]) acc[key] = [];
+        acc[key].push(value);
+      });
+      return acc;
+    }, {} as Record<string, (string | number | boolean | Date)[]>);
   }
 }
 
